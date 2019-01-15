c19c1e4ddebe4f04bcafefd1fed174d9f30a8fa9
Nigel Smyth <nigelsmyth@hotmail.com>
2019-01-15 11:03:16 +0000

    Finished code section of LittleAspNetCore book

--- AspNetCoreTodo/Program.cs
+++ AspNetCoreTodo/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreTodo
 {
@@ -14,8 +15,27 @@ namespace AspNetCoreTodo
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-			//
+            var host = BuildWebHost(args);
+            InitializeDatabase(host);
+            host.Run();
+        }
+
+        private static void InitializeDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    SeedData.InitializeAsync(services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services
+                    .GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Error occurred seeding the DB.");
+                }
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
